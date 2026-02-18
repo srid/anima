e6d697a87c26fcc336d87b017a9d63e6e08d4da7
Sridhar Ratnakumar <srid@srid.ca>
2026-02-18 15:42:18 -0500

    Revamp nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Animation repository for abstract concepts";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
@@ -12,10 +12,9 @@
       perSystem = { config, self', inputs', pkgs, system, ... }: {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            nodejs_20
+            nodejs
             nodePackages.npm
             just
-            nodePackages.live-server
             ffmpeg
           ];
 
